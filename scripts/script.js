f711b8d28753265eca66878cb0d61f1185eaa308
LCF2310 <c0916788@mylambton.ca>
2023-12-06 22:13:07 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// Darshan Gautam, c0916788
+
+// Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
